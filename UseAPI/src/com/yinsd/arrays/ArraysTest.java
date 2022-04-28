845866f5ed100e78fa0c2370993a7803f8b9381c
XY_1949 <y2629262@163.com>
2022-04-28 16:48:26 +0800

    添加Shiro初步使用

---
+++ UseAPI/src/com/yinsd/arrays/ArraysTest.java
@@ -0,0 +1,8 @@
+package com.yinsd.arrays;/**
+ * @author  Yinsd
+ * @date  2022/4/26 22:56
+ * @version 1.0
+ * Only code and time last forever
+ */
+public class ArraysTest {
+}
---
+++ UseAPI/src/com/yinsd/math/demo01/TestDemo.java
@@ -0,0 +1,8 @@
+package com.yinsd.math.demo01;/**
+ * @author  Yinsd
+ * @date  2022/4/26 19:17
+ * @version 1.0
+ * Only code and time last forever
+ */
+public class TestDemo {
+}
